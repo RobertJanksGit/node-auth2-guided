966747f56f462adbac7329b9e75457f6e12a78b3
Robert Jank <robert.jank@yahoo.com>
2024-11-19 10:30:27 -0600

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,14 +1,35 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config");
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
-}
+  let token = req.headers.authorization;
+  if (token) {
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7);
+    }
+    console.log(token);
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, message: `token bad: ${err.message}` });
+      } else {
+        req.decodedJwt = decoded;
+        console.log(req.decodedJwt);
+        next();
+      }
+    });
+  } else {
+    next({ status: 401, message: "what? no token?" });
+  }
+};
 
 // AUTHORIZATION
 const checkRole = (req, res, next) => {
-  next()
-}
+  next();
+};
 
 module.exports = {
   restricted,
   checkRole,
-}
+};
